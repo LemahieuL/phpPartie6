56a67236a21cfead34dc342703d2be8db1b8ead0
Luc <luc.lemahieu@laposte.net>
2018-11-20 12:02:08 +0100

    mise en place des exo

---
+++ exercice1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Exercice 1 php Partie 6</title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
